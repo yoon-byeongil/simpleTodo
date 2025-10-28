0b70a5e47916d9cd9ecd7bd5e1f627b808419609
y101in81 <justbrand101@gmail.com>
2025-10-28 11:44:39 +0900

    apply mvvm

---
+++ simpleTodo/Models/ScheduleItem.swift
@@ -0,0 +1,7 @@
+//
+//  SchduleVar.swift
+//  simpleTodo
+//
+//  Created by 윤병일 on 2025/10/28.
+//
+
---
+++ simpleTodo/ViewModels/View/ScheduleView.swift
@@ -0,0 +1,7 @@
+//
+//  ScheduleView.swift
+//  simpleTodo
+//
+//  Created by 윤병일 on 2025/10/28.
+//
+
